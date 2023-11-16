f522490e34e1b45aa33abfa6a6b96b96cb9f33d9
VictorNivelo <victor.nivelo@unl.edu.ec>
2023-11-16 08:48:30 -0500

    xd

--- src/Controlador/Tda/listas/Exepciones/Vista/VistaRegistrarUsuario.java
+++ src/Controlador/Tda/listas/Exepciones/Vista/VistaRegistrarUsuario.java
@@ -659,6 +659,7 @@ public class VistaRegistrarUsuario extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         CargarVista();
+        cbxTipoIdentificacion.setSelectedIndex(1);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
